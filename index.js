f96c4a7c0f1599d7fe4cd24c3bbe42652e99900d
Gustavo <gstavomota1@gmail.com>
2023-01-22 20:35:34 -0300

    adicionando readSync

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const puppeteer = require('puppeteer');
+const readlineSync = require('readline-sync');
 
 console.log('Bem vindo ao Bot conversor');
 
 async function robo(){
-    const browser = await puppeteer.launch({ headless: false});
+    const browser = await puppeteer.launch({ headless: true});
     const page = await browser.newPage();
-    const moedaBase = 'euro';
-    const moedaFinal = 'real';
+    const moedaBase = readlineSync.question('Moeda Base: ') || 'Dolar';
+    const moedaFinal = readlineSync.question('Moeda Final: ') || 'Real';
+
     const url = `https://www.google.com/search?q=${moedaBase}+para+${moedaFinal}&oq=${moedaBase}+para+${moedaFinal}&aqs=chrome..69i57j0i433i512j0i512l8.9005j1j7&sourceid=chrome&ie=UTF-8`;
     await page.goto(url);
     const resultado = await page.evaluate(() => {
